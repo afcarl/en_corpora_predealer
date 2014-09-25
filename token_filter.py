ef763dcc1497be07482d505b83066e617e99b209
largelymfs <largelymfs@largelymfsdeMacBook-Pro.local>
2014-09-25 14:01:54 +0800

    Add the token filter

---
+++ token_filter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: largelymfs
+# @Date:   2014-09-25 13:51:05
+# @Last Modified by:   largelymfs
+# @Last Modified time: 2014-09-25 14:00:59
+import nltk
+
+
+class TokenFilter:
+
+    def __init__(self, stop=True, min_length=3):
+        self.switch_stop = stop
+        self.switch_min_length = min_length
+        if self.switch_stop:
+            self.stoplist = nltk.corpus.stopwords.words('english')
+
+    def check(self, word):
+        if self.switch_stop:
+            if word in self.stoplist:
+                return False
+        if self.switch_min_length:
+            if len(word) < self.switch_min_length:
+                return False
+
+        return True
+
+    def filter(self, wordlist):
+        return [item for item in wordlist if self.check(item)]
+
+if __name__ == "__main__":
+    import tokenizer
+    line = "I am a student from Tsinghua University"
+    s = TokenFilter()
+    t = tokenizer.Tokenizer()
+
+    print s.filter(t.tokenize(line))
